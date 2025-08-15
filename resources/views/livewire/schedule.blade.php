330c9a5534c9911b7de77063687aab906fd9191d
dmitryBuzz <dmitry@buzzwoo.de>
2025-08-15 19:37:58 +0700

    Update schedule.blade.php

--- resources/views/livewire/schedule.blade.php
+++ resources/views/livewire/schedule.blade.php
@@ -1,32 +1,41 @@
-<x-pulse::card :cols="$cols" :rows="$rows" :class="$class">
+<x-pulse::card :cols="$cols" :rows="$rows" :class="$class" style="min-height: 475px;">
     <x-pulse::card-header name="Schedule">
         <x-slot:icon>
             <x-pulse::icons.command-line />
         </x-slot:icon>
     </x-pulse::card-header>
+
     <x-pulse::scroll :expand="$expand" wire:poll.5s="">
         @empty($events)
             <x-pulse::no-results />
         @else
             <x-pulse::table>
-                <x-pulse::thead>
-                    <tr>
-                        <x-pulse::th>Task</x-pulse::th>
-                        <x-pulse::th>Next Due</x-pulse::th>
-                        <x-pulse::th>Expression</x-pulse::th>
-                    </tr>
-                </x-pulse::thead>
                 <tbody>
-                    @foreach($events as $event)
-                        <tr class="h-2 first:h-0" wire:key="{{ $event['command'] }}-spacer"></tr>
-                        <tr wire:key="{{ $event['command'] }}-row">
-                            <x-pulse::td>
-                                {{ $event['command'] }} <span style="color: #a65f00;">{{ $event['arguments'] }}</span>
-                            </x-pulse::td>
-                            <x-pulse::td>{{ $event['next_due'] }} </x-pulse::td>
-                            <x-pulse::td>{{ $event['expression'] }} {{ $event['repeat_expression'] }}</x-pulse::td>
-                        </tr>
-                    @endforeach
+                @foreach($events as $event)
+                    <tr wire:key="{{ $event['command'] }}-row">
+                        <x-pulse::td style="padding: 0;">
+                            <x-pulse::table class="min-w-full border-collapse">
+                                <tbody>
+                                @foreach([
+                                    'Command' => $event['command'],
+                                    'Description' => $event['description'],
+                                    'File Path' => $event['file_path'],
+                                    'Schedule' => $event['schedule'],
+                                    'Next Run' => $event['next_run'],
+                                    'Timezone' => $event['timezone'],
+                                    'Environment' => $event['environment'],
+                                ] as $key => $value)
+                                    <tr>
+                                        <x-pulse::td class="pr-4" style="width: 150px; min-width: 150px; font-weight: 600; padding: 3px 5px;">{{ $key }}</x-pulse::td>
+                                        <x-pulse::td style="padding: 2px 5px;">{{ $value }}</x-pulse::td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </x-pulse::table>
+                        </x-pulse::td>
+                    </tr>
+                    <tr class="h-2 first:h-0" wire:key="{{ $event['command'] }}-spacer"></tr>
+                @endforeach
                 </tbody>
             </x-pulse::table>
         @endempty
